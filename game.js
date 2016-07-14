27252c2c919a74814ac17db313f8b4b425f40875
Maycon <mayconds000@gmail.com>
2016-07-13 21:28:49 -0300

    add paddle and collisions with the wall

---
+++ game.js
@@ -0,0 +1,68 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+var ballRadius = 10;
+var x = canvas.width/2;
+var y =  canvas.height-30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHendler, false);
+
+function keyDownHandler(e) {
+  if(e.keyCode == 39){
+    rightPressed = true;
+  } else if(e.keyCode == 37){
+    leftPressed = true;
+  }
+}
+
+function keyUpHendler(e) {
+  if(e.keyCode == 39){
+    rightPressed = false;
+  } else if(e.keyCode == 37){
+    leftPressed = false;
+  }
+}
+
+function drawBall(){
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle(){
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw(){
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  x += dx;
+  y += dy;
+
+  // if(y + dy < 0) dy = -dy;
+  // if(y + dy > canvas.height) dy = -dy;
+  if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) dy = -dy;
+  if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) dx = -dx;
+
+  if(rightPressed && paddleX < canvas.width - paddleWidth) paddleX += 7;
+  else if(leftPressed && paddleX > 0) paddleX -= 7;
+
+
+}
+
+setInterval(draw, 10);
